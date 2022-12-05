2a341219ac23624310939d00f42d39fcebaff658
shani kupiec <shani.kupiec@gmail.com>
2022-12-05 16:16:31 +0200

    my first commit

---
+++ src/components/Interests.jsx
@@ -0,0 +1,10 @@
+function Interests() {
+  return (
+    <section className="section bottom">
+        <h3 className="section__title">Interests</h3>
+        <p className="section__txt">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </section>
+  )
+}
+
+export default Interests
